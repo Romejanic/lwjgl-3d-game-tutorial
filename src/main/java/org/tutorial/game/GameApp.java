3f087acdc7cc89451f91284d721dd13cef2212ed
Jack Davenport <jack.davenport5011@gmail.com>
2024-01-10 23:51:32 +1100

    Add LWJGL dependencies

--- src/main/java/org/tutorial/game/GameApp.java
+++ src/main/java/org/tutorial/game/GameApp.java
@@ -1,9 +1,11 @@
 package org.tutorial.game;
 
+import org.lwjgl.Version;
+
 public class GameApp {
     
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("We are running LWJGL v" + Version.getVersion());
     }
 
 }
